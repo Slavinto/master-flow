9f61f8b82b0eaac2e40cbd6a9f80efa201f0c827
Slavinto <slava3669@gmail.com>
2024-03-02 10:10:06 +0300

    footer component light version for services page added

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -13,14 +13,16 @@ const Footer = () => {
         <footer
             className={`${
                 isServicesPage
-                    ? "bg-white text-dark-blue"
+                    ? "bg-white text-dark-blue border-t-2 border-stroke-gray3"
                     : "text-white bg-dark-blue"
             } flex flex-col w-full px-[135px] max-container`}
         >
             <div className='flex content-start py-[70px] justify-between max-w-[1073px]'>
                 <div className='flex flex-col gap-4'>
                     <Image
-                        src='/static/assets/icons/logo-footer.svg'
+                        src={`/static/assets/icons/${
+                            isServicesPage ? "logo.svg" : "logo-footer.svg"
+                        }`}
                         alt='master flow logo'
                         width={206}
                         height={32}
@@ -54,28 +56,62 @@ const Footer = () => {
                     ))}
                 </div>
             </div>
-            <div className='h-[1px] bg-white max-w-[1170px]' />
+            <div
+                className={`h-[1px] ${
+                    isServicesPage ? "bg-darker-blue" : "bg-white"
+                } max-w-[1170px] opacity-30`}
+            />
             <div className='flex justify-between items-center py-10 max-w-[1170px]'>
                 <div className='flex gap-2'>
-                    <div className='flex-center w-12 h-12 border-[1.4px] border-white rounded-full'>
+                    <div
+                        className={`flex-center w-12 h-12 border-[1.4px] ${
+                            isServicesPage
+                                ? "border-darker-blue"
+                                : "border-white"
+                        } rounded-full`}
+                    >
                         <Image
-                            src={"/static/assets/icons/facebook.svg"}
+                            src={`/static/assets/icons/${
+                                isServicesPage
+                                    ? "facebook-dark.svg"
+                                    : "facebook.svg"
+                            }`}
                             alt='facebook icon'
                             width={16}
                             height={16}
                         />
                     </div>
-                    <div className='flex-center w-12 h-12 border-[1.4px] border-white rounded-full'>
+                    <div
+                        className={`flex-center w-12 h-12 border-[1.4px] ${
+                            isServicesPage
+                                ? "border-darker-blue"
+                                : "border-white"
+                        } rounded-full`}
+                    >
                         <Image
-                            src={"/static/assets/icons/twitter.svg"}
+                            src={`/static/assets/icons/${
+                                isServicesPage
+                                    ? "twitter-dark.svg"
+                                    : "twitter.svg"
+                            }`}
                             alt='twitter icon'
                             width={16}
                             height={16}
                         />
                     </div>
-                    <div className='flex-center w-12 h-12 border-[1.4px] border-white rounded-full'>
+                    <div
+                        className={`flex-center w-12 h-12 border-[1.4px] ${
+                            isServicesPage
+                                ? "border-darker-blue"
+                                : "border-white"
+                        } rounded-full`}
+                    >
                         <Image
-                            src={"/static/assets/icons/instagram.svg"}
+                            src={`/static/assets/icons/${
+                                isServicesPage
+                                    ? "instagram-dark.svg"
+                                    : "instagram.svg"
+                            }`}
                             alt='instagram icon'
                             width={16}
                             height={16}
